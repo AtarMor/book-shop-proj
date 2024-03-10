6cac029e125b8a1fabc70a59bc3292c8482eb9bc
Atar Mor <atarmor92@gmail.com>
2024-03-11 00:36:21 +0200

    Refactor BookDetails cmp

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -1,8 +1,11 @@
 export function BookPreview({ book }) {
 	return <article className="book-preview">
 		<h2>{book.title}</h2>
-        <h3>{book.listPrice.amount + book.listPrice.currencyCode +
-        (book.listPrice.isOnSale ? book.listPrice.isOnSale : '')}</h3>
+		<h4>{book.listPrice.amount + ' ' + book.listPrice.currencyCode + 
+		(book.listPrice.isOnSale ? ' On Sale' : '')}</h4>
 		<img src={book.thumbnail} />
+		<div>{book.authors.map(author => {
+			return <h4 key={author}>{author}</h4>
+		})}</div>
 	</article>
 }
