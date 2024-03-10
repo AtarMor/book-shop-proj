f9b42543332f08ab6c8c0e99c285aa0da15490e1
Atar Mor <atarmor92@gmail.com>
2024-03-10 23:28:05 +0200

    Add basic styling

--- cmps/BookList.jsx
+++ cmps/BookList.jsx
@@ -3,12 +3,13 @@ import { BookPreview } from "./BookPreview.jsx"
 export function BookList({books, onSelectBook}) {
 
     if (!books || !books.length) return <div>No books to show</div>
-    return <ul className="book-list">
+    return <ul className="book-list flex wrap">
         {
             books.map(book => <li key={book.id}>
                 <BookPreview book={book} />
-                <div className="book-actions">
-					<button onClick={() => { onSelectBook(book) }}>Select book</button>
+                <div className="book-actions flex justify-between">
+					<button className="show-details-btn" onClick={() => { onSelectBook(book) }}>Select book</button>
+					<button className="remove-btn">X</button>
 				</div>
             </li>)
         }
